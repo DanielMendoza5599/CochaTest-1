d3c0e905818ce83bc94c8db288da5a5bb36fcf2e
DanielMendoza5599 <mendozadaniel5599@gmail>
2019-10-11 15:27:25 -0400

    Se añadio un toast como listener a los botones y se muestran abajo

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -2,14 +2,31 @@ package io.github.n0g4y0.cochatest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var botonVerdad: Button
+    private lateinit var botonFalso: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonVerdad = findViewById<Button>(R.id.verdad_button)
+        botonFalso = findViewById<Button>(R.id.falso_button)
+
+        botonVerdad.setOnClickListener { view: View ->
+            val muestraMensaje = Toast.makeText(this, R.string.correcto_toast, Toast.LENGTH_SHORT)
+            muestraMensaje.show()
+        }
+
+        botonFalso.setOnClickListener { view: View ->
+
+            val muestraMensaje = Toast.makeText(this, R.string.incorrecto_toast, Toast.LENGTH_SHORT)
+            muestraMensaje.show()
+        }
     }
 }
