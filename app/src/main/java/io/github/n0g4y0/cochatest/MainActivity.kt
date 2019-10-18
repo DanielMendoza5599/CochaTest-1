df19f4bda68c92d013a60a720fe2f1c73508ccef
DanielMendoza5599 <mendozadaniel5599@gmail>
2019-10-18 08:05:04 -0400

    Se hizo la tarea para intercambiar la pregunta segun los botones

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -2,33 +2,85 @@ package io.github.n0g4y0.cochatest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var botonVerdad: Button
-    private lateinit var botonFalso: Button
+import kotlinx.android.synthetic.main.activity_main.*
 
+class MainActivity : AppCompatActivity() {
+    private lateinit var botonVerdad : Button
+    private lateinit var botonFalso : Button
+    private lateinit var botonSiguiente : Button
+    private lateinit var botonAnterior : Button
+    private lateinit var texto : TextView
+
+    private var indiceActual = 0
+
+    private val bancoPreguntas = listOf(
+        Pregunta(R.string.question_1, false),
+        Pregunta(R.string.question_2,false),
+        Pregunta(R.string.question_3,true),
+        Pregunta(R.string.question_4,false),
+        Pregunta(R.string.question_5,true),
+        Pregunta(R.string.question_6,false),
+        Pregunta(R.string.question_7,true),
+        Pregunta(R.string.question_8,false)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         botonVerdad = findViewById<Button>(R.id.verdad_button)
         botonFalso = findViewById<Button>(R.id.falso_button)
+        botonSiguiente = findViewById(R.id.botonSiguiente)
+        botonAnterior = findViewById(R.id.botonAnterior)
+        texto = findViewById(R.id.textoPregunta)
 
         botonVerdad.setOnClickListener { view: View ->
-            val muestraMensaje = Toast.makeText(this, R.string.correcto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.setGravity(Gravity.TOP, 0, 130)
-            muestraMensaje.show()
+            verificarRespuesta(true)
         }
 
         botonFalso.setOnClickListener { view: View ->
-
-            val muestraMensaje = Toast.makeText(this, R.string.incorrecto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.setGravity(Gravity.TOP, 0, 130)
-            muestraMensaje.show()
+            verificarRespuesta(false)
         }
+
+        botonSiguiente.setOnClickListener {view: View ->
+            if (indiceActual==7){
+                indiceActual = 0
+            }else{
+                indiceActual = indiceActual + 1
+            }
+            actualizarPregunta()
+        }
+
+        botonAnterior.setOnClickListener {view: View ->
+            if (indiceActual==0){
+                indiceActual = 7
+            }else{
+                indiceActual = indiceActual - 1
+            }
+            actualizarPregunta()
+        }
+    }
+
+    private fun actualizarPregunta(){
+
+        val textoPreguntaID = bancoPreguntas[indiceActual].idTexto
+        texto.setText(textoPreguntaID)
+
+    }
+
+    private fun verificarRespuesta(respuestaUsuario: Boolean){
+
+        val respuestaCorrecta = bancoPreguntas[indiceActual].respuesta
+        val mostrarMensajeID =  if (respuestaUsuario == respuestaCorrecta){
+            "Correcto"
+        } else {
+            "Incorrecto"
+        }
+        Toast.makeText(this,mostrarMensajeID,Toast.LENGTH_SHORT).show()
+
     }
 }
---
+++ app/src/main/java/io/github/n0g4y0/cochatest/Pregunta.kt
@@ -0,0 +1,7 @@
+package io.github.n0g4y0.cochatest
+
+import android.support.annotation.StringRes
+
+data class Pregunta(@StringRes val idTexto : Int, val respuesta: Boolean){
+
+}
